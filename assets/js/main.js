f033b497e69fb76f81d072dd3444e1149751ad2c
eagboy2 <kevinedgley@kevins-MacBook-Pro-7.local>
2016-10-27 13:14:03 -0700

    Add javascript, html and images folder

--- assets/js/main.js
+++ assets/js/main.js
@@ -65,6 +65,32 @@
 
 })(jQuery);
 
+function modify_qty(val) {
+    var qty = document.getElementById('qty').value;
+    var new_qty = parseInt(qty,10) + val;
+    
+    if (new_qty < 0) {
+        new_qty = 0;
+    }
+    
+    document.getElementById('qty').value = new_qty;
+    return new_qty;
+}
+
+function modify_qty2(val) {
+    var qty2 = document.getElementById('qty2').value;
+    var new_qty2 = parseInt(qty2,10) + val;
+    
+    if (new_qty2 < 0) {
+        new_qty2 = 0;
+    }
+    
+    document.getElementById('qty2').value = new_qty2;
+    return new_qty2;
+}
+
+
+
 
 
 
